300d8fe0b48226a7a882c144c66c1b0e2a1c1457
Easton-West <65772616+Easton-West@users.noreply.github.com>
2023-03-07 09:40:25 -0800

    Delete YourMultimeshAssistant/addons/YourMultimeshAssistant directory

--- YourMultimeshAssistant/addons/YourMultimeshAssistant/multimesh_instance.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends MultiMeshInstance3D
-
-@export_node_path("Node3D") var _node_mesh : NodePath : set = _on_set_node
-
-
-func _on_set_node(node: NodePath) -> void:
-	multimesh.mesh = get_node(node).get("mesh")
